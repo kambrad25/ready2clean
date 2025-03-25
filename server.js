1ce782295d0eca272efd221aecb6f4d5be23533d
Kam B <kamronbradford525@gmail.com>
2025-03-25 06:06:30 -0400

    Initial commit

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.use(express.static(path.join(__dirname, "/"), { extensions: ['html']}));
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 // app.use(express.static(__dirname + "/app/public/"));
-app.use(express.static(__dirname + "/public/"))
+app.use(express.static(path.join(__dirname, "/public/")));
 
 
 const connDatabase = (async () => {
